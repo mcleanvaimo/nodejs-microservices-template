f81e7463d3b71c774ce1fcfb381efd80756962bd
mcleanvaimo <58225171+mcleanvaimo@users.noreply.github.com>
2023-12-29 10:50:09 +0000

    7. Gateway API

--- packages/gateway-api/app.js
+++ packages/gateway-api/app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const pino = require('pino-http')();
 
 const router = require('./routes/index');
+const auth = require('./middlewares/auth');
 
 const app = express();
 
@@ -10,8 +11,8 @@ app.use(pino);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(auth);
 
 app.use('/api', router);
 
 module.exports = app;
-
